737a3014caeafcd10e6f4b65159afcf90a45f05d
rryan <rryan@013fbfee-ea42-11dd-a971-7dbc132099af>
2008-12-17 03:33:29 +0000

    Adding metadata model support for video enclave.

---
+++ venclave/admin.py
@@ -0,0 +1,23 @@
+
+from django.contrib import admin
+
+class VideoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date_added'
+    fields = (('General Information',
+               {'fields': ('title', 'video', 'time', 'kind',
+                           'cover_art')}),
+              ('Searching Metadata',
+               {'fields': ('tags', 'visible')}))
+    list_display = ('title', 'time_string', 'date_added', 'visible')
+    list_display_links = ('title',)
+    list_filter = ('visible', 'date_added')
+    search_fields = ('title',)
+
+admin.site.register(VideoAdmin)
+
+
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'pipe', 'last_touched')
+    list_display_links = ('name',)
+
+admin.site.register(ChannelAdmin)

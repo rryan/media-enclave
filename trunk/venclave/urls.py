1107ae79c6e0ddff3bf0c2f23d2f71c5b2889fd9
rryan <rryan@013fbfee-ea42-11dd-a971-7dbc132099af>
2008-12-24 13:39:56 +0000

    Messing with various venclave UI ideas.
    Fixed some bugs so it's now runnable as an app.

--- trunk/venclave/urls.py
+++ trunk/venclave/urls.py
@@ -10,4 +10,28 @@ urlpatterns = patterns(
         {'template': 'index.html'},
         name='venclave-home'),
 
+    url(r'^$',
+        'django.views.generic.simple.direct_to_template',
+        {'template': 'index.html'},
+        name='venclave-search'),
+
+
+    # Static content
+
+    url(r'scripts/(?P<path>.*)$',
+        'django.views.static.serve',
+        {'document_root': 'media/venclave/scripts/'},
+        name='venclave-scripts'),
+
+    url(r'^styles/(?P<path>.*)$',
+        'django.views.static.serve',
+        {'document_root': 'media/venclave/styles/'},
+        name='venclave-styles'),
+
+    url(r'^images/(?P<path>.*)$',
+        'django.views.static.serve',
+        {'document_root': 'media/venclave/images/'},
+        name='venclave-images'),
+
+
 )

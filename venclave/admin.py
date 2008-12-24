fdbe2a4c4e03e1115e2c325522e8a09b62c22239
rryan <rryan@013fbfee-ea42-11dd-a971-7dbc132099af>
2008-12-24 13:39:56 +0000

    Messing with various venclave UI ideas.
    Fixed some bugs so it's now runnable as an app.

--- venclave/admin.py
+++ venclave/admin.py
@@ -1,21 +1,23 @@
 
 from django.contrib import admin
 
-class VideoAdmin(admin.ModelAdmin):
-    date_hierarchy = 'date_added'
-    fields = (('General Information',
-               {'fields': ('title', 'video', 'time', 'kind',
-                           'cover_art')}),
-              ('Searching Metadata',
-               {'fields': ('tags', 'visible')}))
-    list_display = ('title', 'time_string', 'date_added', 'visible')
+from menclave.venclave.models import ContentNode, Channel
+
+class ContentNodeAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created'
+#     fields = (('General Information',
+#                {'fields': ('title', 'path', 'content', 'kind',
+#                            'cover_art')}),
+#               ('Searching Metadata',
+#                {'fields': ('tags', 'visible')}))
+    list_display = ('title', 'created', 'visible')
     list_display_links = ('title',)
-    list_filter = ('visible', 'date_added')
+    list_filter = ('visible', 'created')
     search_fields = ('title',)
 
 class ChannelAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'pipe', 'last_touched')
     list_display_links = ('name',)
 
-admin.site.register(VideoAdmin)
-admin.site.register(ChannelAdmin)
+admin.site.register(ContentNode, ContentNodeAdmin) 
+admin.site.register(Channel, ChannelAdmin)

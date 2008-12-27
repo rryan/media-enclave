25c9e8f71322bd9cfc2357f533a9ec669be990da
rryan <rryan@013fbfee-ea42-11dd-a971-7dbc132099af>
2008-12-27 11:12:06 +0000

    Adding interface plumbing. Some if it is redundant, and much should be torn out later, but it's something to work with.
    It works locally for me -- genres, directors, and content are linked up appropriately.

--- venclave/views.py
+++ venclave/views.py
@@ -1,1 +1,46 @@
-# Create your views here.
+# venclave/views.py
+
+from django.http import Http404
+from django.shortcuts import render_to_response
+
+from menclave.venclave.models import ContentNode, Director, Genre
+
+def simple_search(request):
+    raise Http404()
+
+def content_view(request, id):
+    try:
+        content = ContentNode.objects.get(pk=id)
+    except ContentNode.DoesNotExist:
+        raise Http404()
+
+    return render_to_response("content_view.html",
+                              {'content': content})
+
+def genres_view(request, ids):
+    ids = ids.split(',')
+    try:
+        genres = Genre.objects.in_bulk(ids)
+    except Genre.DoesNotExist:
+        raise Http404()
+
+    return render_to_response("genres_view.html", 
+                              {'genres': genres})
+
+def genre_view(request, id):
+    try:
+        genre = Genre.objects.get(pk=id)
+    except Genre.DoesNotExist:
+        raise Http404()
+
+    return render_to_response("genre_view.html", 
+                              {'genre': genre})
+
+def director_view(request, id):
+    try:
+        director = Director.objects.get(pk=id)
+    except Director.DoesNotExist:
+        raise Http404()
+    
+    return render_to_response("director_view.html",
+                              {'director': director})

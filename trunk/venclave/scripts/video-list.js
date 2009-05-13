afe0a6d89e01d2d4dccda7b947c57ea6b8394a06
SkyeWM <SkyeWM@013fbfee-ea42-11dd-a971-7dbc132099af>
2009-05-13 02:18:24 +0000

    inlined videolist onclick handler for performance purposes

--- trunk/venclave/scripts/video-list.js
+++ trunk/venclave/scripts/video-list.js
@@ -1,31 +1,36 @@
-$(document).ready(function() {
-    $('#video-list').tablesorter()
-    $('#video-list .item-title > a').each(function(i, a) {
-        a = $(a);
-        a.click(function(e) {
-            tr.a.closest('tr').next();
-            tr.toggle();
-            return false;
-        });
-    });
-});
+venclave.videolist = {
+    panes: [],
 
-videolist_onclick = function(e) {
-    toggle_bg()
-    if (!pane) {
-        $.get('{% url venclave-pane %}',
-              {id: id},
-              function(html) {
-                  pane = $(html);
-                  a.closest('tr').after(pane);
-		          var title = pane.find('.nytimes').attr('href').replace(/#/, '');
-                  jQuery.getJSON("http://www.google.com/uds/GwebSearch?v=1.0&q=nytimes review "+encodeURIComponent(title)+"&callback=?",
-                                 function(json) {
-                                     pane.find('.nytimes').attr({'href' : json.responseData.results[0].url});
-                                 });
-              });
-    } else {
-        pane.toggle();
-    }
-    return false;
+    title_onclick: function(a_elt) {
+        var a = $(a_elt);
+        var id = a.closest('tr').attr('id1');
+        var that = this;
+        if (!this.panes[id]) {
+            $.get('/video/load_pane/',
+                  {id: id},
+                  function(html) {
+                      that.toggle_style(a_elt, 'background', '#CCC');
+                      var pane = $(html);
+                      that.panes[id] = pane;
+                      a.closest('tr').after(pane);
+		              var title = pane.find('.nytimes').attr('href').replace(/#/, '');
+                      jQuery.getJSON("http://www.google.com/uds/GwebSearch?v=1.0&q=nytimes review "+encodeURIComponent(title)+"&callback=?",
+                                     function(json) {
+                                         pane.find('.nytimes').attr({'href' : json.responseData.results[0].url});
+                                     });
+                  });
+        } else {
+            this.panes[id].toggle();
+            this.toggle_style(a_elt, 'background', '#CCC');
+        }
+        return false;
+    },
+
+    toggle_style: function(elt, attr, value) {
+        if (elt.style[attr]) {
+            elt.style[attr] = '';
+        } else {
+            elt.style[attr] = value;
+        }
+    },
 }

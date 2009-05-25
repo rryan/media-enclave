3acf1573313e204caffaaee520ee62ff3961267d
reid.kleckner <reid.kleckner@013fbfee-ea42-11dd-a971-7dbc132099af>
2009-05-25 01:42:48 +0000

    Improved video listing performance and fixed the double-pane bug.
    The bug was that when the user clicked twice quickly on the video anchor, two
    pane requests would hit the server and two panes would be loaded.  Now we check
    whether one is loading before firing the XHR.
    
    I improved performance by getting rid of the template rendering in
    create_video_list.  This sucks because it uses less Django niceness, but it's a
    lot faster.  Now all requests on my crappy laptop finish in < 700 ms.
    
    Lastly, I switched from jQuery's .html() method to using replaceHtml as
    described here:
    http://blog.stevenlevithan.com/archives/faster-than-innerhtml
    It's supposed to be even faster than plain innerHTML.

--- trunk/venclave/scripts/base.js
+++ trunk/venclave/scripts/base.js
@@ -1,11 +1,32 @@
-// TODO(rnk): I think Skye meant to commit something here, but I need this in
-// order for shit to work.
 var venclave = {
 
+    /**
+     * Binds 'this' for a function and curries any extra arguments provided.
+     */
     bound_func: function(f, this_) {
+        var args = $.makeArray(arguments);
+        args = args.slice(2);  // Drop the first two args.
         return function() {
-            return f.apply(this_, arguments);
+            return f.apply(this_, args.concat($.makeArray(arguments)));
         };
-    }
+    },
+
+    /**
+     * It turns out that if you want to replace HTML with innerHTML creating a
+     * new node and replacing the old one is a lot faster.
+     *
+     * Instead of writing this:
+     *   el.innerHTML = html;
+     * Write:
+     *   el = venclave.replaceHtml(el, html);
+     *
+     * Source: http://blog.stevenlevithan.com/archives/faster-than-innerhtml
+     */
+    replaceHtml: function(el, html) {
+        var newEl = el.cloneNode(false);
+        newEl.innerHTML = html;
+        el.parentNode.replaceChild(newEl, el);
+        return newEl;
+    },
 
 };
--- trunk/venclave/scripts/video-list.js
+++ trunk/venclave/scripts/video-list.js
@@ -1,29 +1,52 @@
 venclave.videolist = {
-    panes: [],
 
+    /**
+     * A map from id to pane jQuery node.
+     */
+    panes: {},
+
+    /**
+     * Open the pane with the plot summary and metadata when the user clicks on
+     * the movie title anchor.
+     */
     title_onclick: function(a_elt) {
         var a = $(a_elt);
         var id = a.closest('tr').attr('id1');
-        var that = this;
         if (!this.panes[id]) {
-            $.get('/video/load_pane/',
-                  {id: id},
-                  function(html) {
-                      that.toggle_style(a_elt.parentNode, 'background', '#CCC');
-                      var pane = $(html);
-                      that.panes[id] = pane;
-                      a.closest('tr').after(pane);
-		              var title = pane.find('.nytimes').attr('href').replace(/#/, '');
-                      jQuery.getJSON("http://www.google.com/uds/GwebSearch?v=1.0&q=nytimes review "+encodeURIComponent(title)+"&callback=?",
-                                     function(json) {
-                                         pane.find('.nytimes').attr({'href' : json.responseData.results[0].url});
-                                     });
-                  });
-        } else {
+            // Set panes[id] to something while the call is loading so we don't
+            // load two panes if the user clicks twice quickly.
+            this.panes[id] = 'loading';
+            $.get('/video/load_pane/', {id: id},
+                  venclave.bound_func(this.load_pane_success, this, a, id));
+            this.toggle_style(a.get(0).parentNode, 'background', '#CCC');
+        } else if (this.panes[id] != 'loading') {
+            // The pane is already loaded, so toggle it open or closed.
             this.panes[id].toggle();
             this.toggle_style(a_elt.parentNode, 'background', '#CCC');
         }
         return false;
+    },
+
+    /**
+     * Loads the pane below the anchor when the AJAX call returns.
+     */
+    load_pane_success: function(a, id, html) {
+        // Parse the returned HTML into DOM and wrap it with jQuery.
+        var pane = $(html);
+        this.panes[id] = pane;
+        // Put the new pane after the TR wrapping the anchor.
+        a.closest('tr').after(pane);
+        // Fill in the nytimes review link.
+        var title = pane.find('.nytimes').attr('href').replace(/#/, '');
+        var search_url = ("http://www.google.com/uds/GwebSearch?v=1.0" +
+                          "&q=nytimes review " + encodeURIComponent(title) +
+                          "&callback=?");
+        jQuery.getJSON(search_url, function(json) {
+            // Grab the URL of the first search result and call that the
+            // review URL.
+            var review_url = json.responseData.results[0].url;
+            pane.find('.nytimes').attr({'href': review_url});
+        });
     },
 
     toggle_style: function(elt, attr, value) {
@@ -33,4 +56,5 @@ venclave.videolist = {
             elt.style[attr] = value;
         }
     },
+
 }

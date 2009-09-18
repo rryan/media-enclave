901066e74ec5584be06ec145f54c84c65fe3e461
russelljryan <russelljryan@013fbfee-ea42-11dd-a971-7dbc132099af>
2009-09-18 06:25:15 +0000

    Adding log application for Media Enclave

---
+++ trunk/log/views.py
@@ -0,0 +1,14 @@
+
+import json
+
+from django.http import HttpResponse 
+
+from models import RequestLog
+
+def log_json_detail(request, log_id):
+    log = RequestLog.objects.get(pk=log_id)
+    
+    message = {'id': log_id,
+               'data': json.loads(log.message)}
+
+    return HttpResponse(json.dumps(message))

b1ff0caec6c6e22db1e55edee6fd3f380e2ed0c6
reid.kleckner <reid.kleckner@013fbfee-ea42-11dd-a971-7dbc132099af>
2010-09-04 02:22:02 +0000

    Adding initial generated android project.

---
+++ android_apps/AudioEnclave/src/menclave/android/aenclave/AudioEnclaveActivity.java
@@ -0,0 +1,15 @@
+package menclave.android.aenclave;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AudioEnclaveActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

4d09b50c7e0ce28d2265857b67918958dddd72b4
reid.kleckner <reid.kleckner@013fbfee-ea42-11dd-a971-7dbc132099af>
2011-01-06 07:16:02 +0000

    Avoid including menclave.aenclave.urls, since that pulls in all of aenclave, which requires things like pygst which shouldn't be required for running venclave.

--- trunk/urls.py
+++ trunk/urls.py
@@ -6,13 +6,19 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
+    # TODO(rnk): This redirect to /audio/ should be eliminated.  If only one
+    # menclave app is installed, we could pick the right one automatically, and
+    # give a lame picker index if more than one is installed.
     (r'^$', 'django.views.generic.simple.redirect_to', {'url': '/audio'}),
-    (r'^audio/', include('menclave.aenclave.urls')),
-    (r'^video/', include('menclave.venclave.urls')),
-    #(r'^games/', include('menclave.genclave.urls')),
     (r'^log/', include('menclave.log.urls')),
     (r'^admin/(.*)', admin.site.root),
 )
+
+if 'menclave.aenclave' in settings.INSTALLED_APPS:
+    urlpatterns += patterns('', (r'^audio/', include('menclave.aenclave.urls')))
+
+if 'menclave.venclave' in settings.INSTALLED_APPS:
+    urlpatterns += patterns('', (r'^video/', include('menclave.venclave.urls')))
 
 if settings.DEBUG:
     urlpatterns += patterns(

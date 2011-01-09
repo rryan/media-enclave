3a6b3041b8aa3d5be3a310a4305d88c7bc522781
reid.kleckner <reid.kleckner@013fbfee-ea42-11dd-a971-7dbc132099af>
2011-01-09 23:52:04 +0000

    Use eventlet to make metadata scraping much faster.

--- trunk/venclave/metadata/__init__.py
+++ trunk/venclave/metadata/__init__.py
@@ -1,3 +1,7 @@
+# Monkey-patch before we import the scrapers, so they don't import stale stuff.
+import eventlet
+eventlet.monkey_patch()
+
 from common import *
 from imdb import *
 from rottentomatoes import *
@@ -5,27 +9,29 @@ from metacritic import *
 from nyt import *
 
 def update_contentnodes_metadata(nodes, force=False, force_imdb=False,
-                                 force_rt=False, force_mc=False, force_nyt=False):
-    imdb_nodes = nodes.filter(metadata__imdb__imdb_id=None) \
-        if not force and not force_imdb else nodes
-    rt_nodes = nodes.filter(metadata__rotten_tomatoes__rt_id=None) \
-        if not force and not force_rt else nodes
-    mc_nodes = nodes.filter(metadata__metacritic__mc_id=None) \
-        if not force and not force_mc else nodes
-    nyt_nodes = nodes.filter(metadata__nyt_review=None) \
-        if not force and not force_nyt else nodes
+                                 force_rt=False, force_mc=False,
+                                 force_nyt=False):
+    sources = [
+        (force_imdb, 'metadata__imdb__imdb_id',          update_imdb_metadata),
+        (force_rt,   'metadata__rotten_tomatoes__rt_id', update_rottentomatoes_metadata),
+        (force_mc,   'metadata__metacritic__mc_id',      update_metacritic_metadata),
+        (force_nyt,  'metadata__nyt_review',             update_nyt_metadata),
+        ]
 
-    for node in imdb_nodes:
-        update_imdb_metadata(node, force=force or force_imdb)
-    for node in rt_nodes:
-        update_rottentomatoes_metadata(node, force=force or force_rt)
-    for node in mc_nodes:
-        update_metacritic_metadata(node, force=force or force_mc)
-    for node in nyt_nodes:
-        update_nyt_metadata(node, force=force or force_nyt)
+    pool = eventlet.GreenPool(size=100)
+    for (force_source, filter_attr, update_source_metadata) in sources:
+        # If we didn't force updating of this source (or all sources), only
+        # update those that don't have metadata from this source.
+        if not (force or force_source):
+            source_nodes = nodes.filter(**{filter_attr: None})
+
+        for node in source_nodes:
+            pool.spawn(update_source_metadata, node,
+                       force=(force or force_source))
 
 
-def update_metadata(node, force=False, force_imdb=False, force_rt=False, force_mc=False, force_nyt=False):
+def update_metadata(node, force=False, force_imdb=False, force_rt=False,
+                    force_mc=False, force_nyt=False):
     update_imdb_metadata(node, force=force or force_imdb)
     update_rottentomatoes_metadata(node, force=force or force_rt)
     update_metacritic_metadata(node, force=force or force_mc)

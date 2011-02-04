749442d3237518fda6ef6b2f92f5598f644e84a7
reid.kleckner <reid.kleckner@013fbfee-ea42-11dd-a971-7dbc132099af>
2011-02-04 19:08:07 +0000

    More work hacking on the simple browser.  Added recent movies.  Need to work on displaying tv and other content.

--- trunk/venclave/browse.py
+++ trunk/venclave/browse.py
@@ -13,7 +13,12 @@ from menclave.venclave.html import render_to_response
 
 
 def main(request):
-    return render_to_response("venclave/browse.html", request)
+    q = models.ContentNode.with_metadata().order_by('-updated')
+    recent_movies = q.filter(kind=models.KIND_MOVIE)[:10]
+    recent_tv = q.filter(kind=models.KIND_TV)[:10]
+    return render_to_response("venclave/browse.html", request,
+                              {'recent_movies': recent_movies,
+                               'recent_tv': recent_tv})
 
 
 def movies(request):
@@ -21,8 +26,8 @@ def movies(request):
     return render_to_response("venclave/simple_movies.html", request,
                               {'nodes': queryset})
 
+
 def tv(request):
-    # TODO
     queryset = models.ContentNode.objects.filter(kind=models.KIND_SERIES)
     return render_to_response("venclave/browse.html", request)
 
